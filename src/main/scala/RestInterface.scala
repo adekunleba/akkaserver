0f4400873a9b5dfa97f2354e7de1b7f597f36b76
adekunle babatunde <tonytunde2012@gmail.com>
2018-08-24 09:44:46 +0100

    Set to merge clean akk-http to master

--- src/main/scala/RestInterface.scala
+++ src/main/scala/RestInterface.scala
@@ -3,17 +3,11 @@ import akka.http.scaladsl.server.Route
 import db.RedisService
 import exceptionhandler.MyImplicitExceptionHandler
 import resources.TensorFlowResources
-import service.ImageService
 
 trait RestInterface extends Resources with MyImplicitExceptionHandler{
-  println("RestInterface being initialized")
-  // This doesn't have definition, so until Main hits the below line
-  // > implicit val executionContext = system.dispatcher
-  // this is left null
+
   implicit def executionContext: ExecutionContext
 
-  //This is where declared imageService from TensorFlowResource is now being initialized.
-  lazy val imageService = new ImageService
   lazy val redisModelService = new RedisService
   val routes: Route =  imageRoutes
 }
--- src/main/scala/resources/TensorFlowResources.scala
+++ src/main/scala/resources/TensorFlowResources.scala
@@ -5,38 +5,19 @@ import akka.http.scaladsl.unmarshalling.{MultipartUnmarshallers, Unmarshaller}
 import db.RedisService
 import entities.Image
 import routing.MyResource
-import service.ImageService
-
 
 
 trait TensorFlowResources extends MyResource {
   println("TensorFlowResources being initialized")
 
-  //ImageService needs implicit ExecutionContext at initialization, but when the below line is called from RestInterface,
-  //ExecutionContext is not instantiated, since the below ...
-
-  //ORIGINAL EXECUTION APPROACH
-  //val imageService = new ImageService
-
-
-  //NEW OBSERVATION:
-  // Image Service was never supposed to initialized initially, it was supposed to be initialized inside of Tensorflow
-  //Resources. After this is declared here, it is instantiated in RestInterface Trait and since it is lazy val
-  // it only comes when needed and this will be when there is a proper execution context..
-  //val imageService: ImageService
-  // ... is called beofore the following line inside Main
-  //  > implicit val executionContext = system.dispatcher
   val redisModelService : RedisService
 
   def imageRoutes: Route = pathPrefix("images") {
     pathEnd {
       post {
         entity(as[Image]) { image =>
-          //println(s"imageService = $imageService")
-          println(s"image = $image")
           completeWithLocationHeader(
             resourceId = redisModelService.set(image),
-            //resourceId = imageService.createEntity(image),
             ifDefinedStatus = 201, ifEmptyStatus = 409
           )
         }
